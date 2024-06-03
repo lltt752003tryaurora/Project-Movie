e0e3e83502829dbcc33e86e8eabf63f231f500f2
Le Long Truong Thinh <truongthinhkl1@gmail.com>
2024-06-03 20:59:05 +0700

    token

--- src/util/constant/settingSystem.js
+++ src/util/constant/settingSystem.js
@@ -1,6 +1,6 @@
 export const URL_DOMAIN = "https://movienew.cybersoft.edu.vn";
 export const TokenCybersoft =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOb2RlSlMgMzciLCJIZXRIYW5TdHJpbmciOiIyNi8wNS8yMDI0IiwiSGV0SGFuVGltZSI6IjE3MTY2ODE2MDAwMDAiLCJuYmYiOjE2OTg1MTI0MDAsImV4cCI6MTcxNjgyOTIwMH0.WRdYDUhhNdFlNs99AP2dpjnCOrvxtAV4BH7IvNMVa_Y";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0OCIsIkhldEhhblN0cmluZyI6IjE0LzAzLzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxMDM3NDQwMDAwMCIsIm5iZiI6MTY3OTY3NzIwMCwiZXhwIjoxNzEwNTIyMDAwfQ.jxAwiJsyx50zaFwQ53GBO30YWu2JqHbVcw9VaYIch9s";
 
 export const TOKEN = "access_token";
 
